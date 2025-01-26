681ec00a46d8e67b8e55e2592d753dc5ab309022
AstralScience <thepursuiter2@gmail.com>
2025-01-26 21:48:59 +0800

    Create installation_steps.sh

---
+++ installation_steps.sh
@@ -0,0 +1,7 @@
+pip install matplotlib
+pip install cohere
+pip install openmeteo-requests
+pip install requests-cache retry-requests numpy pandas
+pip install pyperclip
+
+# PLEASE NOTE, THE TERMINAL COMMANDS RAN ABOVE WERE RAN IN THE SIMULATED TERMINAL IN JETBRAINS PYCHARM COMMUNITY EDITION
